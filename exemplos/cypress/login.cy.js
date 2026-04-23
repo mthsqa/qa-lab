8ee6ade7065f087c521138d1eda0ef9f920a603a
mthsqa <matheus_jooe@hotmail.com>
2026-04-23 07:02:55 -0300

    test: preencher credenciais invalidas no Cypress

--- exemplos/cypress/login.cy.js
+++ exemplos/cypress/login.cy.js
@@ -1,3 +1,5 @@
 describe('login', () => {
   it('exibe erro com senha inválida', () => {
     cy.visit('/login')
+    cy.get('[data-testid=email]').type('qa@example.com')
+    cy.get('[data-testid=password]').type('errada')
